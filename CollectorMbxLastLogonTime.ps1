755d6b184d6cb5525e4b841bf56ad2c31ecf28e0
Glaucia Diniz <pop.glaucia@gmail.com>
2023-02-16 13:56:32 -0300

    Update CollectorMbxLastLogonTime.ps1

--- CollectorMbxLastLogonTime.ps1
+++ CollectorMbxLastLogonTime.ps1
@@ -2,13 +2,28 @@
 Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
 Set-ADServerSettings -ViewEntireForest $true
 
-
+<#
 #Servidores
 $ServerSearch = 'Name','SRV-EXCH13-01','SRV-EXCH13-02'
 
 
 $AllMailboxes = Get-Mailbox -ResultSize Unlimited | ? {($ServerSearch -contains $_.ServerName)} | Select-Object DisplayName,Alias,SamAccountName,UserPrincipalName,PrimarySmtpAddress,RecipientTypeDetails,OrganizationalUnit
 $Total = $AllMailboxes.count
+#>
+
+$AllMailboxes = Get-Recipient -ResultSize "Unlimited" | Select-Object `
+    DisplayName, `
+    Alias, `
+    SamAccountName, `
+    @{n='UserPrincipalName';e={$((Get-User $($_.SamAccountName)).UserPrincipalName)}}, `
+    PrimarySmtpAddress, `
+    @{n='EmailAddresses';e={($_."EmailAddresses".ProxyAddressString -join ", ")}}, `
+    @{n='ExternalEmailAddress';e={($_."ExternalEmailAddress".ProxyAddressString )}}, `
+    RecipientTypeDetails, `
+    OrganizationalUnit
+$Total = $AllMailboxes.count
+
+Write-Host "Total de contas de usuarios no catalogo de endereços --> [$([int]$Total)]"
 
 
 $Report = [System.Collections.Generic.List[Object]]::new()
@@ -21,10 +36,12 @@ ForEach ($Mailboxes in $AllMailboxes) {
     $SamAccountName = @($Mailboxes.SamAccountName);
     $UserPrincipalName = @($Mailboxes.UserPrincipalName);
     $PrimarySmtpAddress = @([String]$Mailboxes.PrimarySmtpAddress.Address);
+    $EmailAddresses = @([String]$Mailboxes.EmailAddresses);
+    $ExternalEmailAddress = @([String]$Mailboxes.ExternalEmailAddress -replace "SMTP:",'');
     $RecipientTypeDetails = @($Mailboxes.RecipientTypeDetails);
     $OrganizationalUnit = @($Mailboxes.OrganizationalUnit);
     
-    if (!(Get-MailboxStatistics -Identity $([String]$PrimarySmtpAddress)).LastLogonTime)
+    if (!(Get-MailboxStatistics -Identity $([String]$PrimarySmtpAddress) -ErrorAction SilentlyContinue).LastLogonTime)
     {
     Write-Host "login null - [$Contador], aguarde até [$Total]"
         $ReportLine = [PSCustomObject]@{
@@ -33,6 +50,8 @@ ForEach ($Mailboxes in $AllMailboxes) {
         SamAccountName = $($SamAccountName)
         UserPrincipalName = $($UserPrincipalName)
         PrimarySmtpAddress = $($PrimarySmtpAddress)
+        EmailAddresses = $($EmailAddresses)
+        ExternalEmailAddress = $($ExternalEmailAddress)
         RecipientTypeDetails = $($RecipientTypeDetails)
         LastLogonTime = $($Null)
         TotalItemSize = $((Get-MailboxStatistics -Identity $([String]$PrimarySmtpAddress) -ErrorAction SilentlyContinue).TotalItemSize)
@@ -50,6 +69,8 @@ ForEach ($Mailboxes in $AllMailboxes) {
         SamAccountName = $($SamAccountName)
         UserPrincipalName = $($UserPrincipalName)
         PrimarySmtpAddress = $($PrimarySmtpAddress)
+        EmailAddresses = $($EmailAddresses)
+        ExternalEmailAddress = $($ExternalEmailAddress)
         RecipientTypeDetails = $($RecipientTypeDetails)
         LastLogonTime = $([String](Get-MailboxStatistics -Identity $([String]$PrimarySmtpAddress) -ErrorAction SilentlyContinue).LastLogonTime.ToString('dd/MM/yyyy HH:mm:ss'))
         TotalItemSize = $((Get-MailboxStatistics -Identity $([String]$PrimarySmtpAddress) -ErrorAction SilentlyContinue).TotalItemSize)
@@ -70,3 +91,5 @@ ForEach ($Mailboxes in $AllMailboxes) {
 $Diretorio = "D:\ExchangeLogCollector\CollectorMbx\"
 mkdir $Diretorio
 $Report | Select-Object * | Export-Csv -Path $($Diretorio + "CollectorMbx_LastLogin.csv") -NoTypeInformation -Encoding UTF8
+
+Write-Host "Contas do catalogo exportadas para --> [$($Diretorio)]"
