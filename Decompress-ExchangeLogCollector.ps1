18219cf89a1f5b0f03b02e6d3cd8079e65b1d1ea
Glaucia Diniz <pop.glaucia@gmail.com>
2023-03-14 13:14:08 -0300

    Update Decompress-ExchangeLogCollector.ps1

--- Decompress-ExchangeLogCollector.ps1
+++ Decompress-ExchangeLogCollector.ps1
@@ -1,20 +1,22 @@
-#######################################################################################################
 [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 
-$result = [System.Windows.Forms.MessageBox]::Show('O Script ExchangeLogCollector foi executado a partir de uma maquina remota?' , "Tipo de Coleta" , 4)
+$Directory = [Microsoft.VisualBasic.Interaction]::InputBox('Informe o diretorio onde o dados serao descompactados. O diretório não pode ser uma subpasta. Siga o Exemplo:', 'Decompress Path', "D:\ExchangeLogCollector")
+
+$result = [System.Windows.Forms.MessageBox]::Show('O Script ExchangeLogCollector foi executado a partir de um Servidor Exchange?' , "Tipo de Coleta" , 4)
 
 if ($result -eq 'Yes')
     {
     $ServerSearch = @()
-    $ServerSearch = [Microsoft.VisualBasic.Interaction]::InputBox('Quais servidores Exchange receberam a coleta. Exemplo:', 'Exchange Servers', "'SRV-EXCH13-01','SRV-EXCH13-02'")
+    $ServerSearch = [Microsoft.VisualBasic.Interaction]::InputBox('Quais servidores Exchange receberam a coleta. Exemplo:', 'Exchange Servers', "'SRV-EXCH-01','SRV-EXCH-02'")
+    $ServerSearch = $(($ServerSearch -split ",") -replace "'",'')
     Write-Host "yes - Remote Connection"
     $ServerSearch | % {
     $RemoteSession = New-PSSession -ComputerName $ServerSearch
     }
 }else{
     $ServerSearch = @()
-    $ServerSearch = [Microsoft.VisualBasic.Interaction]::InputBox('Quais servidores Exchange receberam a coleta. Exemplo:', 'Exchange Servers', "'SRV-EXCH13-01','SRV-EXCH13-02'")
+    $ServerSearch = [Microsoft.VisualBasic.Interaction]::InputBox('Quais servidores Exchange receberam a coleta. Exemplo:', 'Exchange Servers', "'SRV-EXCH-01','SRV-EXCH-02'")
     $ComputerName = "ComputerName"
     $ServerSearch = $("'ComputerName',$ServerSearch")
     $ServerSearch = $(($ServerSearch -split ",") -replace "'",'')
@@ -24,21 +26,46 @@ if ($result -eq 'Yes')
 #######################################################################################################
 
 $Folder  = [String](Get-Date -Format yyyyMd)
+$zipFolder  = [String](Get-Date -Format Md)
 
 $Contador = 0
+
 
 $RemoteSession | % {
     $Contador++
 
-    Copy-Item -FromSession $($_) –Path "$('D:\ExchangeLogCollector\' + $Folder + '\' + $_.ComputerName + '-' + $zipFolder + '.zip')" –Destination "$('D:\ExchangeLogCollector\' + $Folder + '\')" 
-    Expand-Archive -path "$('D:\ExchangeLogCollector\' + $Folder + '\' + $_.ComputerName + '-' + $zipFolder + '.zip')" -destinationpath "$('D:\ExchangeLogCollector\' + $Folder + '\' + $_.ComputerName + '-' + $zipFolder)"
-    $Archives = Get-ChildItem "$('D:\ExchangeLogCollector\' + $Folder + '\' + $_.ComputerName + '-' + $zipFolder)" | ? {($_.Name -like "*.zip")}
-    
-    foreach($Archive in $Archives){
-        $FullName = $($Archive.FullName)
-        $Directory = $($Archive.FullName -replace ".zip",'')
 
-        Expand-Archive -path "$($FullName)" -destinationpath "$($Directory)"
+    if (Get-Item -Path "$($Directory + '\' + $Folder + '\' + $_.ComputerName + '-' + $zipFolder + '.zip')" -ErrorAction SilentlyContinue)
+    {
+        Write-Host "$Contador - File found ... descompress $($_.ComputerName).zip"
+        
+        Expand-Archive -path "$($Directory + '\' + $Folder + '\' + $_.ComputerName + '-' + $zipFolder + '.zip')" -destinationpath "$($Directory + '\' + $Folder + '\' + $_.ComputerName + '-' + $zipFolder)"
+
+        $Archives = Get-ChildItem "$($Directory + '\' + $Folder + '\' + $_.ComputerName + '-' + $zipFolder)" | ? {($_.Name -like "*.zip")} 
+        
+        foreach($Archive in $Archives){
+            $FullName = $($Archive.FullName)
+            $destinationpath = $($Archive.FullName -replace ".zip",'')
+
+            Expand-Archive -path "$($FullName)" -destinationpath "$($destinationpath)"
+        }
     }
-    Write-Host "$Contador - Copy file server .. $($_.ComputerName)"
-}
+    else
+    {
+        Write-Host "$Contador - Copy and descompress  file server .. $($_.ComputerName)"
+
+        Copy-Item -FromSession $($_) –Path "$($Directory + '\' + $Folder + '\' + $_.ComputerName + '-' + $zipFolder + '.zip')" –Destination "$($Directory + '\' + $Folder + '\')" 
+        
+        Expand-Archive -path "$($Directory + '\' + $Folder + '\' + $_.ComputerName + '-' + $zipFolder + '.zip')" -destinationpath "$($Directory + '\' + $Folder + '\' + $_.ComputerName + '-' + $zipFolder)"
+        
+        $Archives = Get-ChildItem "$($Directory + '\' + $Folder + '\' + $_.ComputerName + '-' + $zipFolder)" | ? {($_.Name -like "*.zip")} 
+            
+            foreach($Archive in $Archives){
+            $FullName = $($Archive.FullName)
+            $destinationpath = $($Archive.FullName -replace ".zip",'')
+
+            Expand-Archive -path "$($FullName)" -destinationpath "$($destinationpath)"
+        }
+    }
+
+} 
